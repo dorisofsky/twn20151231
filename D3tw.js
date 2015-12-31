7bd3d16fd3ba02102cdc14d43d96037b9bf348e8
dorisofsky <dorisofsky@gmail.com>
2015-12-31 11:20:09 +0800

    臺灣地圖基本款完成

--- D3tw.js
+++ D3tw.js
@@ -1,8 +1,6 @@
 d3.json("http://dorisofsky.github.io/taiwan_realtime2/county.json", function(topodata) {
-      var features = topojson.feature(topodata, topodata.objects["county"]).features;
-      // 這裡要注意的是 topodata.objects["county"] 中的 "county" 為原本 shp 的檔名
-
-   var path = d3.geo.path().projection( // 路徑產生器
+  var features = topojson.feature(topodata, topodata.objects["county"]).features;
+  var path = d3.geo.path().projection( // 路徑產生器
     d3.geo.mercator().center([121,24]).scale(6000) // 座標變換函式
   );
   d3.select("svg").selectAll("path").data(features)
